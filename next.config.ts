9ab3ac657ec927c2ceaa78da90f7e826fa2a5026
mahdian <mahdian@dorsacloud.com>
2025-10-10 12:28:03 +0330

    fix: detach from parent folder yarn.lock file

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  outputFileTracingRoot: __dirname,
 };
 
 export default withNextIntl(nextConfig);
