8f092bd2186210951603eec203d77a08976fdf8f
mahdian <mahdian@dorsacloud.com>
2025-12-05 22:03:52 +0330

    migrate from middleware.ts file into the proxy.ts file due to use next.js doc

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,9 +1,8 @@
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./i18n/routing";
 
-export default createMiddleware(routing);
+export const proxy = createMiddleware(routing);
 
 export const config = {
-  // Match only internationalized pathnames
   matcher: ["/", "/(fa|en)/:path*", "/((?!api|trpc|_next|_vercel|.*\\..*).*)"],
 };
